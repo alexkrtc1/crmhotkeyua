54bde783004911e20294d4227458b7aa66a6ec5d
alexkrtc1 <alexkrtc1@gmail.com>
2022-10-20 10:46:46 +0300

    corrected version

--- biko_load_comments/__manifest__.py
+++ biko_load_comments/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "BIKO: Import comments to opportunities",
-    'version': '14.0.0.0.4',
+    'version': '14.0.1.4.0',
     'author': 'Borovlev AS and Hotkey',
     'company': 'BIKO,HOTKEY',
     "depends": ['crm'],
