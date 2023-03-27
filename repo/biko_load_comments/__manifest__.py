4704f686786d777bcfff3597500a78bc79839a41
alexkrtc1 <alexkrtc1@gmail.com>
2023-03-27 14:34:28 +0300

    clearing

--- repo/biko_load_comments/__manifest__.py
+++ repo/biko_load_comments/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
     'name': "BIKO: Import comments to opportunities",
-    'version': '14.0.0.1.4',
-    'summary': 'Import comments and activities to opportunities,leads. Last Update 16.03.2023',
-    'description': 'Import comments and activities to opportunities,leads. Last Update 16.03.2023',
+    'version': '14.0.0.1.5',
+    'summary': 'Import comments and activities to opportunities,leads. Last Update 27.03.2023',
+    'description': 'Import comments and activities to opportunities,leads. Last Update 27.03.2023',
     'author': 'Borovlev AS and Hotkey',
     'company': 'BIKO,HOTKEY',
     "depends": ['base', 'crm'],
